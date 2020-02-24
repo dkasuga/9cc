fd9d93653a8bd240e798a07f8452a31607249cd0
Daisuke Kasuga <benjamin.at.mac@gmail.com>
2020-02-25 01:53:33 +0900

    Add "if" statement

--- codegen.c
+++ codegen.c
@@ -1,5 +1,7 @@
 #include "chibi.h"
 
+static int labelseq = 1;
+
 // Pushes the given node's address to the stack
 static void gen_addr(Node* node)
 {
@@ -47,6 +49,28 @@ static void gen(Node* node)
         gen(node->rhs); // 右辺
         store();
         return;
+    case ND_IF: {
+        int seq = labelseq++;
+        if (node->els) { // if(A) B else C
+            gen(node->cond);
+            printf("  pop rax\n");
+            printf("  cmp rax, 0\n");
+            printf("  je  .L.else.%d\n", seq);
+            gen(node->then);
+            printf("  jmp .L.end.%d\n", seq);
+            printf(".L.else.%d:\n", seq);
+            gen(node->els);
+            printf(".L.end.%d:\n", seq);
+        } else { // if(A) B
+            gen(node->cond);
+            printf("  pop rax\n");
+            printf("  cmp rax, 0\n");
+            printf("  je  .L.end.%d\n", seq);
+            gen(node->then);
+            printf(".L.end.%d:\n", seq);
+        }
+        return;
+    }
     case ND_RETURN:
         gen(node->lhs);
         printf("  pop rax\n");
