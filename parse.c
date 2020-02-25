bb5ba38384a320cc8711148c41e26786812e00ac
Daisuke Kasuga <benjamin.at.mac@gmail.com>
2020-02-25 14:22:52 +0900

    Add Block

--- parse.c
+++ parse.c
@@ -95,6 +95,8 @@ static Node* read_expr_stmt(void)
 // stmt = "return" expr ";"
 //      | "if" "(" expr ")" stmt ("else" stmt)?
 //      | "while" "(" expr ")" stmt
+//      | "for" "(" expr? ";" expr? ";" expr? ")" stmt
+//      | "{" stmt* "}"
 //      | expr ";"
 static Node* stmt(void)
 {
@@ -140,6 +142,20 @@ static Node* stmt(void)
             expect(")");
         }
         node->then = stmt();
+        return node;
+    }
+
+    if (consume("{")) {
+        Node head = {};
+        Node* cur = &head;
+
+        while (!consume("}")) {
+            cur->next = stmt();
+            cur = cur->next;
+        }
+
+        Node* node = new_node(ND_BLOCK);
+        node->body = head.next;
         return node;
     }
 
